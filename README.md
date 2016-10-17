85b7f4bfe0148f7805235af892404e291897f290
Ian Henderson <iancurtish@gmail.com>
2016-10-17 12:32:06 -0700

    Added to readme and script

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Wikipedia-Game
 The `wikipedia_game(root, target)` function uses a non-recursive breadth-first search algorithm to find the shortest path between two Wikipedia pages. The `root` and `target` arguments must be the exact titles of the desired pages. 
 
+## Requirements
+1. Python3
+2. virtualenv
+
 ## Setup
 1. Set up a `Python3` virtual environment with `virtualenv` using the command `virtualenv example_environment --python='python3'`.
 2. Activate the `virtualenv` environment with the command `source example_environment/bin/activate`.
--- wikipedia_game.py
+++ wikipedia_game.py
@@ -1,9 +1,12 @@
-import wikipedia
+import os, wikipedia
 
 
 # Non-recursive Breadth-first Search Implementation
-def wikipedia_game(root, target):
-    print("Finding path between %s and %s\n" % (root, target))
+def wikipedia_game():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Wikipedia Game\n\n")
+    root = input("Start page title: ")
+    target = input("Target page title: ")
     root_page = wikipedia.page(root)
     G = {}
     G[root_page.title] = {
@@ -12,20 +15,49 @@ def wikipedia_game(root, target):
         'parent': None
     }
     Q = [G[root_page.title]]
+    print("\nParsing Wikipedia...")
     while Q:
         current = Q[0]
         Q = Q[1:]
-        current_page = wikipedia.page(current['title'])
-        print(current_page.title)
-        for link in current_page.links:
-            if link not in G:
-                G[link] = {
-                    'title': link,
-                    'distance': current['distance'] + 1,
-                    'parent': current
-                }
-                if link == target:
-                    print("Found", target + "!")
-                    print("Data:", G[link])
-                    return
-                Q.append(G[link])
+        try:
+            current_page = wikipedia.page(current['title'])
+            print("\t%s" % current_page.title)
+            for link in current_page.links:
+                if link not in G:
+                    G[link] = {
+                        'title': link,
+                        'distance': current['distance'] + 1,
+                        'parent': current
+                    }
+                    if link == target:
+                        print("\tFound %s!\n\t\tData: %s" % (link, G[link]))
+                        return
+                    Q.append(G[link])
+        except wikipedia.exceptions.PageError as e:
+            # Skips over the item in the queue if it results in a page error.
+            print("\tSkipping %s...\n\t\t%s" % (current['title'], e))
+        except wikipedia.exceptions.DisambiguationError as e:
+            # Disambiguation Page
+            G[e.title] = {
+                'title': e.title,
+                'distance': current['distance'] + 1,
+                'parent': current
+            }
+            # Adds every link on disambiguation page to queue
+            for option in e.options:
+                if option not in G:
+                    G[option] = {
+                        'title': option,
+                        'distance': current['distance'] + 2,
+                        'parent': G[e.title] 
+                    }
+                    if option == target:
+                        print("\tFound %s!\n\t\tData: %s" % (option, G[option]))
+                        return
+                    Q.append(G[option])
+        except KeyboardInterrupt:
+            print("Exiting")
+            return
+
+
+wikipedia_game()
