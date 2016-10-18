ee3f223b7f2abb24a3d1e78840e11aacb4f49513
Ian Henderson <iancurtish@gmail.com>
2016-10-18 15:48:17 -0700

    fixed "

--- wikipedia_game.py
+++ wikipedia_game.py
@@ -11,10 +11,10 @@ def print_path(data):
 # Non-recursive Breadth-first Search Implementation
 def wikipedia_game():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Wikipedia Game\n\n")
-    root = input("Start page title: ")
+    print('Wikipedia Game\n\n')
+    root = input('Start page title: ')
     root_page = wikipedia.page(root)
-    target = input("Target page title: ")
+    target = input('Target page title: ')
     target_page = wikipedia.page(target)
     G = {}
     G[root_page.title] = {
@@ -23,13 +23,13 @@ def wikipedia_game():
         'parent': None
     }
     Q = [G[root_page.title]]
-    print("\nParsing Wikipedia...")
+    print('\nParsing Wikipedia...')
     while Q:
         current = Q[0]
         Q = Q[1:]
         try:
             current_page = wikipedia.page(current['title'])
-            print("\t%s" % current_page.title)
+            print('\t%s' % current_page.title)
             for link in current_page.links:
                 if link not in G:
                     G[link] = {
@@ -68,9 +68,9 @@ def wikipedia_game():
                     Q.append(G[option])
         except wikipedia.exceptions.PageError as e:
             # Skips over the item in the queue if it results in a page error.
-            print("\tSkipping %s...\n\t\t%s" % (current['title'], e))
+            print('\tSkipping %s...\n\t\t%s' % (current['title'], e))
         except KeyboardInterrupt:
-            print("Exiting")
+            print('Exiting')
             return
 
 
