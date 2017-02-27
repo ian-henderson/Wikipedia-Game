a0100d76f352dbd2bb4ebcba3ad4260b3cde2343
Ian Henderson <iancurtish@gmail.com>
2017-02-27 09:08:26 -0800

    asdf

--- wikipedia_game.py
+++ wikipedia_game.py
@@ -1,89 +1,58 @@
 import os
 import sys
-
 import wikipedia
 
 
 def get_algorithm():
-
-    print('Enter selection for an algorithm.')
-    print('\t(1) Breadth-first Search')
-    print('\t(2) Depth-first Search')
-    print('\t(3) Iterative Deepening Depth-first Search')
-    print('\n\tSelection: ')
+    algorithm = None
+    while algorithm not in range(1, 4):
+        if algorithm:
+            print('%s wasn\'t an option. Try again.' % algorithm)
+        print('\nEnter selection for an algorithm.')
+        print('\t1) Breadth-first Search')
+        print('\t2) Depth-first Search')
+        print('\t3) Iterative Deepening Depth-first Search')
+        algorithm = int(input('\nSelection: '))
+    return algorithm
 
 
 def get_page(page_type):
-
     page = None
-
     while not page:
-
         try:
             entry = input('Enter %s page title: ' % page_type)
             page = wikipedia.page(entry)
-
         except wikipedia.exceptions.DisambiguationError as e:
-            print('\nDisambiguation Selection (Choose one of these or use\
-                    another term)')
+            print('\nDisambiguation Selection (Choose one of these or use another term)')
             for option in e.options:
                 print('\t' + option)
             print()
-
         except wikipedia.exceptions.PageError:
             print('Page error, try again.')
-
         except KeyboardInterrupt:
             print('Exiting')
             sys.exit()
-
     return page
 
 
 def print_path(data):
-
     if data['parent']:
         print_path(data['parent'])
         print(' => ', end='')
-
     print(data['title'], end='')
 
 
 # https://en.wikipedia.org/wiki/Breadth-first_search
-def breadth_first_search():
-
-
-# https://en.wikipedia.org/wiki/Depth-first_search
-def depth_first_search():
-
-
-# https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search
-def iterative_deepining_depth_first_search():
-
-
-def wikipedia_game():
-
-    # Clears up the screen on start.
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    print('Wikipedia Game\n\n')
-
-    root_page = get_page('root')
-    target_page = get_page('target')
-
-    # TODO: test to see if G = {} is needed.
+def breadth_first_search(root_page, target_page):
     G = {}
     G[root_page.title] = {
         'title': root_page.title,
         'distance': 0,
         'parent': None
     }
-
     Q = [G[root_page.title]]
-
     print('\nFinding the path between the %s and %s pages...'
           % (root_page.title, target_page.title))
-
     while Q:
         current = Q[0]
         Q = Q[1:]
@@ -103,7 +72,7 @@ def wikipedia_game():
                         print_path(G[link])
                         print()
                         sys.exit()
-                        Q.append(G[link])
+                    Q.append(G[link])
         except wikipedia.exceptions.DisambiguationError as e:
             # Disambiguation Page
             G[e.title] = {
@@ -125,13 +94,36 @@ def wikipedia_game():
                         print_path(G[option])
                         print()
                         sys.exit()
-                        Q.append(G[option])
+                    Q.append(G[option])
         except wikipedia.exceptions.PageError as e:
             # Skips over the item in the queue if it results in a page error.
             print('\tSkipping %s...\n\t\t%s' % (current['title'], e))
         except KeyboardInterrupt:
             print('Exiting')
             sys.exit()
+
+
+# https://en.wikipedia.org/wiki/Depth-first_search
+# def depth_first_search():
+
+
+# https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search
+# def iterative_deepining_depth_first_search():
+
+
+def wikipedia_game():
+    # Clears up the screen on start.
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Wikipedia Game\n\n')
+    root_page = get_page('root')
+    target_page = get_page('target')
+    algorithm = get_algorithm()
+    if algorithm == 1:
+        breadth_first_search(root_page, target_page)
+    # else if algorithm == 2:
+        # depth_first_search(root_page, target_page)
+    # else if algorithm == 3:
+        # iterative_deepining_depth_first_search(root_page, target_page)
 
 
 wikipedia_game()
